80440a372dac5da04491101d921ba1c4b3617001
aalhadbah <aalhadbah@mail.csuchico.edu>
2018-02-12 21:05:52 -0800

    middle step

--- main.cpp
+++ main.cpp
@@ -9,16 +9,23 @@ int main()
 {
 
 string FirstName, LastName;
-int Value;                      //Declaring Variables
+char fInitial, lInitial;                      //Declaring Variables
 
 
 cout<<"What's your first name?\n";           
 cin>> FirstName;                             //Sentences For Names
+fInitial=FirstName[0]; 
+
 
 cout<<"what's your last name?\n";
-cin>> LastName;  
-  
-  
+cin>> LastName;
+lInitial=LastName[0];
+
+
+
+
+cout<< "Welcome, "<<fInitial<<"."<<lInitial<<", here is your fortune...\n";
+
   
   
   //tell fortune
